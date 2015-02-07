5dcf183446e91194300c63f0c9ab6b6ba4db34a2
Vance Anderson <vanceanderson41@gmail.com>
2015-02-06 19:46:16 -0700

    Really trying to track down why nothing displays on the page

--- src/main/webapp/admin/adminDao.php
+++ src/main/webapp/admin/adminDao.php
@@ -56,57 +56,57 @@ class adminDao
 			die( mysql_error() . "</body></html>" ); 
 		} 
 		return $result;
-	}
+	// }
 
-	// assumes that the paramaters passed are well formed csv
-	public function setFilterQuery($makes, $models, $years, $alternates) {
-		$query = "insert into filterquery values(0, '" . $makes . "', '" . $models . "', '" . $years . "', '" . $alternates . "')";
-		if ( !( $result = mysql_query( $query, $this->database ) ) ) 
-		{
-			$query = "UPDATE filterquery SET makes='" . $makes ."', models='" . $models . "', years='" . $years . "', alternates='" . $alternates . "' WHERE filterid=0"; 
-			if ( !( $result = mysql_query( $query, $this->database ) ) ) 
-			{
-				print( "Could not execute query! <br />" ); 
-				die( mysql_error() . "</body></html>" ); 
-			}
-		} 
-	}
+	// // assumes that the paramaters passed are well formed csv
+	// public function setFilterQuery($makes, $models, $years, $alternates) {
+	// 	$query = "insert into filterquery values(0, '" . $makes . "', '" . $models . "', '" . $years . "', '" . $alternates . "')";
+	// 	if ( !( $result = mysql_query( $query, $this->database ) ) ) 
+	// 	{
+	// 		$query = "UPDATE filterquery SET makes='" . $makes ."', models='" . $models . "', years='" . $years . "', alternates='" . $alternates . "' WHERE filterid=0"; 
+	// 		if ( !( $result = mysql_query( $query, $this->database ) ) ) 
+	// 		{
+	// 			print( "Could not execute query! <br />" ); 
+	// 			die( mysql_error() . "</body></html>" ); 
+	// 		}
+	// 	} 
+	// }
 
-	public function display($result, $displayHeader=true)
-	{
-		if ($displayHeader)
-		{
-			print("<tr>");
-		    for ( $index = 0; $index < mysql_num_fields($result); $index++ )
-		    {
-		        $header = mysql_field_name($result, $index);
-		        print("<th>$header</td>");
-		    }
-		    print("</tr>");
-		}
+	// public function display($result, $displayHeader=true)
+	// {
+	// 	if ($displayHeader)
+	// 	{
+	// 		print("<tr>");
+	// 	    for ( $index = 0; $index < mysql_num_fields($result); $index++ )
+	// 	    {
+	// 	        $header = mysql_field_name($result, $index);
+	// 	        print("<th>$header</td>");
+	// 	    }
+	// 	    print("</tr>");
+	// 	}
 	    
-	    // fetch each record in result set 
-	    for ( $counter = 0; $row = mysql_fetch_row( $result ); $counter++ ) 
-	    { 
-	        // build table to display results 
-	        print( "<tr>" ); 
-	        foreach ( $row as $key => $value ) 
-	            print( "<td>$value</td>" ); 
+	//     // fetch each record in result set 
+	//     for ( $counter = 0; $row = mysql_fetch_row( $result ); $counter++ ) 
+	//     { 
+	//         // build table to display results 
+	//         print( "<tr>" ); 
+	//         foreach ( $row as $key => $value ) 
+	//             print( "<td>$value</td>" ); 
 
-	        print( "</tr>" ); 
-	    } // end for 
-	}
+	//         print( "</tr>" ); 
+	//     } // end for 
+	// }
 
-	//This is mostly for test functions that need to do things like execute delete queries etc. Try not to use this method in non-test code
-	public function runQuery($query)
-	{
-		if ( !( $result = mysql_query( $query, $this->database ) ) ) 
-		{ 
-			print( "Could not execute query! <br />" ); 
-			die( mysql_error() . "</body></html>" ); 
-		} 
-		return $result;
-	}
+	// //This is mostly for test functions that need to do things like execute delete queries etc. Try not to use this method in non-test code
+	// public function runQuery($query)
+	// {
+	// 	if ( !( $result = mysql_query( $query, $this->database ) ) ) 
+	// 	{ 
+	// 		print( "Could not execute query! <br />" ); 
+	// 		die( mysql_error() . "</body></html>" ); 
+	// 	} 
+	// 	return $result;
+	// }
 
 
 }
