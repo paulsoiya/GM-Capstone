0df3b25dd3d8d1cd1862abd971237805b840bb46
psoiya <paul@econceptmedia.com>
2015-02-12 17:59:08 -0700

    PS02122015A added client validation to registration form

--- src/main/webapp/scripts/register.js
+++ src/main/webapp/scripts/register.js
@@ -1,16 +1,55 @@
 $( "#registerForm" ).submit(function( event ) {
   event.preventDefault();
   
+  var firstName = $("#first_name").val();
+  var lastName = $("#last_name").val();
+  var email = $("#email").val();
+  var confirmEmail = $("#confirm_email").val();
+  var password = $("#password").val();
+  var confirmPassword = $("#confirm_password").val();
+  var position = $("#position").val();
+  var reason = $("#reason").val();
+  
+  
+  
+ //validate form
+ if(!isValidLength(firstName, 1) ){
+     $("#failure-message").html("You must enter your first name.");
+     $("#failure").show();
+    return;
+ }else if(!isValidLength(lastName, 1) ){
+     $("#failure-message").html("You must enter your last name.");
+     $("#failure").show();
+    return;
+ }else if(!isValidEmail(email)){
+     $("#failure-message").html("You must enter a valid email address.");
+     $("#failure").show();
+    return;
+ }else if(!isMatching(email, confirmEmail)){
+     $("#failure-message").html("The emails you entered do not match.");
+     $("#failure").show();
+     return;
+ }else if(!isValidLength(password, 5)){
+     $("#failure-message").html("Your password must be at least 6 characters.");
+     $("#failure").show();
+     return;
+ }else if(!isMatching(password, confirmPassword)){
+     $("#failure-message").html("The passwords you entered do not match.");
+     $("#failure").show();
+     return;
+ }
+
 
   
-  var postFields = {first_name: $("#first_name").val(),
-                    last_name: $("#last_name").val(),
-                    email: $("#email").val(),
-                    confirm_email: $("#confirm_email").val(),
-                    password: $("#password").val(),
-                    password_confirm: $("#password_confirm").val(),
-                    position: $("#position").val(),
-                    reason: $("#reason").val()};
+  var postFields = {first_name: firstName,
+                    last_name: lastName,
+                    email: email,
+                    confirm_email: confirmEmail,
+                    password: password,
+                    password_confirm: confirmPassword,
+                    position: position,
+                    reason: reason};
+
 
    $.ajax({   
             type: "POST",
@@ -19,7 +58,6 @@ $( "#registerForm" ).submit(function( event ) {
             url: "api/pending-users",   
             success: function(data){
                 
-                console.log(data);
                 if(data.result === "success"){
                     $("#success-message").html("Your request was submitted successfully.");
                     $("#failure").hide();
