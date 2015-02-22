937bfa4e5b8160437cdf5e8014b96c3ac31fa673
Vance Anderson <vanceanderson41@gmail.com>
2015-02-22 04:59:12 -0700

    Bugfix: If user clicks "Add {type}" and the field is empty, no DB change should be made.

--- src/main/webapp/admin/update.php
+++ src/main/webapp/admin/update.php
@@ -7,7 +7,7 @@
 	if(isset($_POST['newModel'])) {
 		$newModel = $_POST['newModel'];
 		$currentModels = $dao->getModelString($currentMake);
-		if(strlen($currentModels > 0)) {
+		if(strlen($currentModels) > 0 && strlen($newModel) > 0) {
 			$currentModels .= ",";
 		}
 		$currentModels .= $newModel;
@@ -17,7 +17,7 @@
 	if(isset($_POST['newAlternate'])) {
 		$newAlternate = $_POST['newAlternate'];
 		$currentAlternates = $dao->getAlternateString($currentMake);
-		if(strlen($currentAlternates) > 0) {
+		if(strlen($currentAlternates) > 0  && strlen($newAlternate) > 0) {
 			$currentAlternates .= ",";
 		}
 		$currentAlternates .= $newAlternate;
