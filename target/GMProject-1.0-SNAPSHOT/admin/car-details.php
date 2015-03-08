d566711bd5a97630fd1e983741889f1504a5d004
unknown <sdmcelro@asu.edu>
2015-03-08 12:58:38 -0700

    properties file for databases added

---
+++ target/GMProject-1.0-SNAPSHOT/admin/car-details.php
@@ -0,0 +1,40 @@
+<?php
+    require_once('adminDao.php');
+    
+    $newMake = "";
+    $dao = new adminDao('filterquery');
+    if(isset($_GET['newMake'])) {
+    	$newMake = $_GET['newMake'];
+    }
+    else {
+    	$newMake = "chevrolet";
+    }
+    $result = $dao->getMakeFilterQuery();
+	$models = $dao->getModelsFilterQuery($newMake);
+    $alternates = $dao->getAlternatesFilterQuery($newMake);
+    $modelRow = mysql_fetch_assoc($models);
+    $alternateRow = mysql_fetch_assoc($alternates);
+    $modelString = '';
+    $alternateString = '' ;
+    $modelString .= $modelRow['models'];
+    if(isset($_GET['newModel'])) {
+    	$modelString .= ",";
+    	$modelString .= $_GET['newModel'];
+    }
+    
+    $alternateString .= $alternateRow['alternates'];
+    if(isset($_GET['newAlternate'])) {
+		$alternateString .= ",";
+    	$alternateString .= $_GET['newAlternate'];
+    }
+   $row = mysql_fetch_assoc($result);
+                   	$makes = preg_split('/[,]/', $row['makes']);
+                   /* foreach($makes as $make) {
+                    	echo "<option value=" . $make . " " . ($make == $newMake ? "selected>" : ">") . $make . "</option>";  
+                    }*/
+$arr = array("models" => $modelString,
+             "alt" => $alternateString);
+echo json_encode($arr);
+
+
+?>
