7d02fcda580e7f9ec952bf82a9e06627b87c4f86
Becca Little <rebecca.little@asu.edu>
2015-03-14 15:49:54 -0700

    removing autogenerated stuff from project

--- target/GMProject-1.0-SNAPSHOT/scripts/login.js
+++
@@ -1,7 +0,0 @@
-/* 
- * This javascript file will handle the login form.
- * It will contain an ajax call that calls the servlet that processes the login
- * form.
- */
-
-
--- target/GMProject-1.0-SNAPSHOT/scripts/splash.js
+++
@@ -1,6 +0,0 @@
-
-
-// attach onclick to register button
-$("#registerBtn").click(function() {
-    window.open("registration.html", "_self");
-});
