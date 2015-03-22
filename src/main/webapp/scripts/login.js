0090aa5d0f9da882487280f1b1c4d0f608af3ea2
psoiya <paul@econceptmedia.com>
2015-03-21 18:05:42 -0700

    Added login.js and register.js code'

--- src/main/webapp/scripts/login.js
+++ src/main/webapp/scripts/login.js
@@ -1,7 +1,52 @@
 /* 
  * This javascript file will handle the login form.
- * It will contain an ajax call that calls the servlet that processes the login
+ * It will contain an ajax call that calls the REST endpoint that processes the login
  * form.
  */
 
 
+$("#loginForm").submit(function(event) {
+    event.preventDefault();
+    //hide any previous errors
+    $("#failure").hide();
+    
+    var email = $("#login_email").val();
+    var password = $("#login_password").val();
+
+    var postFields = {
+        email: email,
+        password: password,
+    };
+
+    $.ajax({
+        type: "POST",
+        data: postFields,
+        cache: false,
+        url: "api/users/authenticate",
+        success: function(data) {
+                    
+            if (data.result === "true") {
+                console.log("success");
+                if(data.admin == "false"){
+                 //redirect to admin page
+                 
+                }else{
+                 //redirect to user page   
+                }
+            } else {
+                console.log("failure");
+                //display error
+                $("#failure-message").html("The username/password you entered is incorrect");
+                $("#failure").show();
+                
+            }
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            console.log(textStatus);
+             $("#failure-message").html("There was a problem attempting to log you in. Please try again.");
+             $("#failure").show();
+        }
+    });
+
+    return false;
+});
