4ca4e6f0249ac0cbf8def892eefa6bbd1e42c32f
Vance Anderson <vanceanderson41@gmail.com>
2015-03-25 20:23:30 -0700

    Addition of remove functionality for explicit words

--- src/main/webapp/inside/admin/scripts/updateExplicit.js
+++ src/main/webapp/inside/admin/scripts/updateExplicit.js
@@ -19,23 +19,49 @@ $( "#explicitForm" ).submit(function( event ) {
             dataType: 'JSONP',
             data: postFields,
             cache: false,  
-            url: "api/analytics",   
+            url: "../../api/analytics",   
             success: function(data){
                 
                 if(data.result === "success"){
                     $("#success-message").html("Your request was submitted successfully.");
                     $("#failure").hide();
                     $("#success").show();
+                    
                 }else{
                     $("#failure-message").html("There was a problem submitting your request. Please Try again.");
                     $("#failure").show();
                 }
             }   
         });   
-        
+    populateExisting();
+    return false;
+});
+
+$( "#removeForm" ).submit(function( event ) {
+  event.preventDefault();
+  
+  var selector = document.getElementById("removeSelector");
+  var theId = selector[selector.selectedIndex].id;
+  console.log(theId + "");
+
+  var postFields = {id: theId};
+  
+
+   $.ajax({   
+            type: "DELETE",
+            dataType: 'JSONP',
+            data: postFields,
+            cache: false,  
+            url: "../../api/analytics/" + theId,   
+            success: function(data){
+                
+            }   
+        });   
+    populateExisting();
     return false;
 });
 
 function isValidLength(val, len){
     return (val.length > len ? true : false);
 }
+
