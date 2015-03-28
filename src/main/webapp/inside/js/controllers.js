2a4490b710b7b0c369d79ccfde48fdeea5a5f2a4
psoiya <paul@econceptmedia.com>
2015-03-28 09:39:30 -0700

    updated manage users

--- src/main/webapp/inside/js/controllers.js
+++ src/main/webapp/inside/js/controllers.js
@@ -45,23 +45,84 @@ controllers.controller('AdminCtrl', ['$scope',
 
 controllers.controller('ManageUsersCtrl', ['$scope', '$http', function ($scope, $http){
 
-  $http.get('http://localhost:7001/GMProject/api/pending-users').success(function(data) {
-        $scope.pusers = data.pendingUser;
-    });
+  $scope.getPendingUsers = function () {
+	  $http.get('http://localhost:7001/GMProject/api/pending-users').success(function(data) {
+		  $scope.pusers = data.pendingUser;
+	  });
+  }
     
-    //fill the users table
-    $http.get('http://localhost:7001/GMProject/api/users').success(function(data) {
-        
-        //change boolean value for admin to 
-        //textual representation of user role
-        for(var i = 0; i < data.user.length; i++){
-            if(data.user[i].admin == "true")
-                data.user[i].admin = "Admin";
-            else
-                data.user[i].admin = "User";
-        }
-        $scope.users = data.user;
-    });
+  $scope.getUsers = function () {
+	  $http.get('http://localhost:7001/GMProject/api/users').success(function(data) {
+		  //change boolean string value for admin to 
+		  //textual representation of user role
+		  for(var i = 0; i < data.user.length; i++){
+			  if(data.user[i].admin == "true")
+				  data.user[i].admin = "Admin";
+			  else
+				  data.user[i].admin = "User";
+		  }
+		  $scope.users = data.user;
+	  });
+  }
+  
+  $scope.getPendingUsers();
+  $scope.getUsers();
+  
+  $scope.grantAdminAccess = function(id){
+	  console.log("HI");
+	  console.log("id",id);
+	  
+	  console.log($scope.users[id]);
+  }
+  
+  /**
+   * Deletes a registered user
+   */
+  $scope.removeAccess = function(id){
+	  var uid = $scope.users[id].id;
+	  $http.delete('http://localhost:7001/GMProject/api/users/' + uid).success(function (data, status) {
+          console.log(data);
+          console.log("DELETED");
+          $scope.getUsers();
+      });
+  }
+  
+  /**
+   * Converts a pending user into a user
+   */
+  $scope.grantAccess = function(id){
+	
+	  var postFields = {email: $scope.pusers[id].email, 
+			  			password: $scope.pusers[id].password, 
+			  			first_name: $scope.pusers[id].firstName, 
+			  			last_name: $scope.pusers[id].lastName,
+			  			puser_id: $scope.pusers[id].id
+			  			};
+	  $http({
+		    method: 'POST',
+		    url: 'http://localhost:7001/GMProject/api/users',
+		    data: $.param(postFields),
+		    headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+	  }).success(function(data, status, headers, config) {
+			  console.log(data);
+			  $scope.getPendingUsers();
+			  $scope.getUsers();
+	  }).error(function(data, status, headers, config) {
+			console.log("Something went wrong");
+	  });
+  }
+  
+  /**
+   * Deletes a pending user from the database
+   */
+  $scope.deleteRequest = function(id){
+	  var uid = $scope.pusers[id].id;
+	  $http.delete('http://localhost:7001/GMProject/api/pending-users/' + uid).success(function (data, status) {
+          console.log(data);
+          console.log("DELETED");
+          $scope.getPendingUsers();
+      });
+  }
 
 }]);
 
