565e0dca39920428c0d2bd3c5a18c9a9827cccdd
psoiya <paul@econceptmedia.com>
2015-04-09 13:23:22 -0700

    create users sql script now has a hashed password

--- src/main/resources/sql/insertTestUsers.sql
+++ src/main/resources/sql/insertTestUsers.sql
@@ -1,12 +1,12 @@
 INSERT INTO user (password, salt, email, fname, lname, create_date, is_admin)
 VALUES 
-('pass','a','a@a.com','Rob','Miller', '9999-12-31 23:59:59', '0'),
-('pass','b','b@a.com','Becca','Little', '9999-12-31 23:59:59', '0'),
-('pass','c','c@a.com','AJ','Bansal', '9999-12-31 23:59:59', '0'),
-('pass','d','d@a.com','Ashish','Amresh', '9999-12-31 23:59:59', '0'),
-('pass','e','e@a.com','Kevin','Gary', '9999-12-31 23:59:59', '0'),
-('pass','f','f@a.com','Paul','Soiya', '9999-12-31 23:59:59', '1'),
-('pass','g','g@a.com','Sean','McElroy', '9999-12-31 23:59:59', '1'),
-('pass','h','h@a.com','Vance','Anderson', '9999-12-31 23:59:59', '1'),
-('pass','i','i@a.com','Nick','Rynearson', '9999-12-31 23:59:59', '1'),
-('pass','j','j@a.com','Arbi','Ghazarian', '9999-12-31 23:59:59', '1');
+('5f4dcc3b5aa765d61d8327deb882cf99','a','a@a.com','Rob','Miller', '9999-12-31 23:59:59', '0'),
+('5f4dcc3b5aa765d61d8327deb882cf99','b','b@a.com','Becca','Little', '9999-12-31 23:59:59', '0'),
+('5f4dcc3b5aa765d61d8327deb882cf99','c','c@a.com','AJ','Bansal', '9999-12-31 23:59:59', '0'),
+('5f4dcc3b5aa765d61d8327deb882cf99','d','d@a.com','Ashish','Amresh', '9999-12-31 23:59:59', '0'),
+('5f4dcc3b5aa765d61d8327deb882cf99','e','e@a.com','Kevin','Gary', '9999-12-31 23:59:59', '0'),
+('5f4dcc3b5aa765d61d8327deb882cf99','f','f@a.com','Paul','Soiya', '9999-12-31 23:59:59', '1'),
+('5f4dcc3b5aa765d61d8327deb882cf99','g','g@a.com','Sean','McElroy', '9999-12-31 23:59:59', '1'),
+('5f4dcc3b5aa765d61d8327deb882cf99','h','h@a.com','Vance','Anderson', '9999-12-31 23:59:59', '1'),
+('5f4dcc3b5aa765d61d8327deb882cf99','i','i@a.com','Nick','Rynearson', '9999-12-31 23:59:59', '1'),
+('5f4dcc3b5aa765d61d8327deb882cf99','j','j@a.com','Arbi','Ghazarian', '9999-12-31 23:59:59', '1');
