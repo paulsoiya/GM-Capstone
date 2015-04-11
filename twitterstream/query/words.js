54c8cb67c720bd36f0b8c7f636f1250ff6d93877
Becca Little <rebecca.little@asu.edu>
2015-04-11 13:29:59 -0700

    Sean says query folder can be deleted

--- twitterstream/query/words.js
+++
@@ -1,7 +0,0 @@
-function(doc){
-  //if(doc.tweettime > 0 && doc.tweettime < 1426936843000  && doc.tweetlocation = "Arizona" ){
-    for (var key in doc){
-      emit(key, doc[key]);
-    }
-  //}
-} 
