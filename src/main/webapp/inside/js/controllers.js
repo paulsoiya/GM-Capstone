5a4e28b904d5f6eed1c6196df44b0b9509336178
Starshot <starshot@cox.net>
2015-04-10 23:52:35 -0700

    fix file path updated file names

--- src/main/webapp/inside/js/controllers.js
+++ src/main/webapp/inside/js/controllers.js
@@ -332,8 +332,7 @@ controllers.controller('QueryCtrl',['$scope', '$http', '$filter', function($scop
 			barChart.datasets[0].data[0] = wordCountData[0][1];
 			barChart.datasets[0].data[1] = wordCountData[1][1];
 			barChart.datasets[0].data[2] = wordCountData[2][1];
-			barChart.datasets[0].data[3] = wordCountData[3][1];
-*/
+			barChart.datasets[0].data[3] = wordCountData[3][1];*/
 			var barData = {
 				labels: [wordCountData[0][0], 
 					wordCountData[1][0], 
@@ -369,45 +368,9 @@ controllers.controller('QueryCtrl',['$scope', '$http', '$filter', function($scop
 			//barChart.update();
 			//pieChart.update();
 
-			$scope.HeatMapTest = function(){
+			$scope.heatmap = function(){
 
-				var myLatlng = new google.maps.LatLng(25.6586, -80.3568);
-// map options,
-var myOptions = {
-  zoom: 3,
-  center: myLatlng
-};
-// standard map
-map = new google.maps.Map(document.getElementById("HeatMapPanel"), myOptions);
-
-// heatmap layer
-heatmap = new HeatmapOverlay(map, 
-  {
-    // radius should be small ONLY if scaleRadius is true (or small radius is intended)
-    "radius": 2,
-    "maxOpacity": 1, 
-    // scales the radius based on map zoom
-    "scaleRadius": true, 
-    // if set to false the heatmap uses the global maximum for colorization
-    // if activated: uses the data maximum within the current map boundaries 
-    //   (there will always be a red spot with useLocalExtremas true)
-    "useLocalExtrema": true,
-    // which field name in your data represents the latitude - default "lat"
-    latField: 'lat',
-    // which field name in your data represents the longitude - default "lng"
-    lngField: 'lng',
-    // which field name in your data represents the data value - default "value"
-    valueField: 'count'
-  }
-);
-
-var testData = {
-  max: 8,
-  data: [{lat: 24.6408, lng:46.7728, count: 3},{lat: 50.75, lng:-1.55, count: 1}, ...]
-};
-
-heatmap.setData(testData);
-
+map = new google.maps.Map(document.getElementById("Map"), myOptions);
 			}
 		});
   }
