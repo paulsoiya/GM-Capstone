f9902013c6f4056d9ad92fb97299c9d94e2b814e
Starshot <starshot@cox.net>
2015-04-18 16:19:40 -0700

    Added dynamic letter change function

--- src/main/webapp/inside/js/controllers.js
+++ src/main/webapp/inside/js/controllers.js
@@ -58,7 +58,7 @@ controllers.controller('AdminCtrl', ['$scope',
   }]);
 
 controllers.controller('ManageAnalyticsCtrl', ['$scope', '$http', function($scope, $http) {
-  
+
   $scope.getExplicit = function() {
     $http.get('../api/explicit-content').success(function(data) {
       $scope.explicitContents = data;
@@ -69,10 +69,10 @@ controllers.controller('ManageAnalyticsCtrl', ['$scope', '$http', function($scop
       $scope.commonContents = data;
     });
   }
-  
+
   $scope.getExplicit();
   $scope.getCommon();
-  
+
   $scope.newExplicit = '';
   $scope.removeExplicit = '';
   $scope.addExplicit = function(){
@@ -130,10 +130,10 @@ controllers.controller('ManageUsersCtrl', ['$scope', '$http', function ($scope, 
 		  $scope.pusers = data;
 	  });
   }
-    
+
   $scope.getUsers = function () {
 	  $http.get('../api/users').success(function(data) {
-		  //change boolean value for admin to 
+		  //change boolean value for admin to
 		  //textual representation of user role
 		  for(var i = 0; i < data.length; i++){
 			  if(data[i].admin)
@@ -144,21 +144,21 @@ controllers.controller('ManageUsersCtrl', ['$scope', '$http', function ($scope, 
 		  $scope.users = data;
 	  });
   }
-  
+
   $scope.getPendingUsers();
   $scope.getUsers();
-  
+
   $scope.changeRole = function(id, admin){
 	  var uid = $scope.users[id].id;
 	  var payload;
-	  
-	  if ( admin ) { 
+
+	  if ( admin ) {
 		  payload = {role: true};
 	  } else {
 		  payload = {role: false};
 	  }
-	  
-	  
+
+
 	  $http({
 		    method: 'PUT',
 		    url: "../api/users/"+ uid + "/role",
@@ -168,9 +168,9 @@ controllers.controller('ManageUsersCtrl', ['$scope', '$http', function ($scope, 
 		  console.log(data);
           $scope.getUsers();
 	  });
-	 
+
   }
-  
+
   /**
    * Deletes a registered user
    */
@@ -181,15 +181,15 @@ controllers.controller('ManageUsersCtrl', ['$scope', '$http', function ($scope, 
           $scope.getUsers();
       });
   }
-  
+
   /**
    * Converts a pending user into a user
    */
   $scope.grantAccess = function(id){
-	
-	  var postFields = {email: $scope.pusers[id].email, 
-			  			password: $scope.pusers[id].password, 
-			  			first_name: $scope.pusers[id].firstName, 
+
+	  var postFields = {email: $scope.pusers[id].email,
+			  			password: $scope.pusers[id].password,
+			  			first_name: $scope.pusers[id].firstName,
 			  			last_name: $scope.pusers[id].lastName,
 			  			puser_id: $scope.pusers[id].id
 			  			};
@@ -206,7 +206,7 @@ controllers.controller('ManageUsersCtrl', ['$scope', '$http', function ($scope, 
 			console.log("Something went wrong");
 	  });
   }
-  
+
   /**
    * Deletes a pending user from the database
    */
@@ -227,31 +227,31 @@ controllers.controller('ManageUsersCtrl', ['$scope', '$http', function ($scope, 
 
 controllers.controller('NavbarCtrl', ['$scope', '$state',
   function ($scope, $state){
-    
+
     $scope.userIsAdmin = true;
-    
+
     $scope.isAdminState = function(){
-      return $state.includes("admin"); 
+      return $state.includes("admin");
     }
   }]);
 
 controllers.controller('ProfileCtrl',['$scope','$http', function($scope, $http){
-	
+
 	$scope.getUserDetail = function() {
-		
+
 		 $http.get('../api/users/' + _uToken).success(function(data) {
 			  $scope.userData = data;
 		  });
 	}
-	
+
 	$scope.getUserDetail();
-	
-	
+
+
 	$scope.save = function()
 	  {
 		$("#success").hide();
 		$("#failure").hide();
-		
+
 	   var postFields = {	first_name:$("#FirstName").val(),
 				  			last_name:$("#LastName").val(),
 				  			email:$("#inputEmail").val(),
@@ -263,8 +263,8 @@ controllers.controller('ProfileCtrl',['$scope','$http', function($scope, $http){
 		   $("#failure").show();
 		   return;
 	   }
-	   
-	   
+
+
 		  $http({
 			    method: 'PUT',
 			    url: 'http://localhost:7001/GMProject/api/users/' + _uToken,
@@ -274,7 +274,7 @@ controllers.controller('ProfileCtrl',['$scope','$http', function($scope, $http){
 		      console.log(data);
 		      $("#success").hide();
 		      $("#failure").hide();
-		      
+
 				  if(data.result === "success"){
 				      $("#success-message").html("Your changes have been saved");
 				      $("#success").show();
@@ -283,14 +283,14 @@ controllers.controller('ProfileCtrl',['$scope','$http', function($scope, $http){
 				      $("#failure-message").html("There was a problem saving your changes");
 				      $("#failure").show();
 				  }
-				  
+
 		  }).error(function(data, status, headers, config) {
 		      $("#success-message").hide();
 		      $("#failure-message").hide();
 
 		      $("#failure-message").html("There was a problem saving your changes");
 		      $("#failure").show();
-				
+
 		  });
 	  }
 }]);
@@ -300,18 +300,18 @@ controllers.controller('ProfileCtrl',['$scope','$http', function($scope, $http){
 
 controllers.controller('QueryCtrl',['$scope', '$http', '$filter', function($scope, $http, $filter){
 
-  
+
   // Location dropdown
-  $scope.locations = ['All Locations', 'Alabama', 'Alaska', 'Arizona', 'Arkansas', 
-                      'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida', 
-                      'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 
-                      'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 
-                      'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 
-                      'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 
-                      'New Jersey', 'New Mexico', 'New York', 'North Carolina', 
-                      'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 
-                      'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 
-                      'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 
+  $scope.locations = ['All Locations', 'Alabama', 'Alaska', 'Arizona', 'Arkansas',
+                      'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida',
+                      'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa',
+                      'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland',
+                      'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi',
+                      'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
+                      'New Jersey', 'New Mexico', 'New York', 'North Carolina',
+                      'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania',
+                      'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee',
+                      'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington',
                       'West Virginia', 'Wisconsin', 'Wyoming'];
   // Datepickers
   $scope.open = function($event, dateType) {
@@ -328,7 +328,7 @@ controllers.controller('QueryCtrl',['$scope', '$http', '$filter', function($scop
   $scope.endDate = $scope.today;
   $scope.aMonthAgo = $filter('date')(new Date() - 2592000000, 'yyyy-MM-dd');
   $scope.startDate = $scope.aMonthAgo;
-  
+
   // Make, model, year
   // makes
   $http.get('http://localhost:7001/GMProject/api/makes').success(function(data) {
@@ -345,11 +345,11 @@ controllers.controller('QueryCtrl',['$scope', '$http', '$filter', function($scop
       $scope.years = data;
 	  console.log(data);
   });
-  
+
 
   //POST, query response
   $scope.queryPost = function() {
-    $http.post('http://localhost:7001/GMProject/api/query', 
+    $http.post('http://localhost:7001/GMProject/api/query',
 	    {
 			msg: "location="+$scope.selectLocation+"&"+
 			"endDate="+$scope.endDate+"&"+
@@ -364,9 +364,9 @@ controllers.controller('QueryCtrl',['$scope', '$http', '$filter', function($scop
 			var responseJSON = JSON.parse(data.result);
 			var wordCount = JSON.parse(responseJSON.wordCount);
 			var sentiment = JSON.parse(responseJSON.sentiment);
-			
+
 			var wordCountData = [];
-			
+
 			for(var i = 0; i < wordCount.rows.length; ++i){
 				if(wordCount.rows[i].key !== "_id" &&
 				wordCount.rows[i].key !== "_rev" &&
@@ -376,14 +376,14 @@ controllers.controller('QueryCtrl',['$scope', '$http', '$filter', function($scop
 					wordCountData.push([wordCount.rows[i].key, wordCount.rows[i].value]);
 				}
 			}
-			
+
 			wordCountData.sort(function(current, next) {
 				return ((current[1] > next[1]) ? -1 : ((current[1] === next[1]) ? 0 : 1));
 			});
-			
+
 			console.log("wordCountData");
 			console.log(wordCountData);
-			
+
 			if(wordCountData.length > 30){
 				var tempData = [];
 				for(var i = 0; i < 30; ++i){
@@ -391,22 +391,22 @@ controllers.controller('QueryCtrl',['$scope', '$http', '$filter', function($scop
 				}
 				wordCountData = tempData;
 			}
-			
+
 			//Word Cloud
-			WordCloud(document.getElementById('wordCloud_canvas'), 
-				{ 
-					list: wordCountData, 
+			WordCloud(document.getElementById('wordCloud_canvas'),
+				{
+					list: wordCountData,
 					color: 'random-dark',
 					shape: 'square',
 					rotateRatio: 0.0,
 					weightFactor: 2
 				}
 			);
-  			
+
 			console.log(sentiment.rows[0].value[0]);
 			console.log(Math.abs(sentiment.rows[0].value[0] + 1));
 			console.log(Math.round(Math.abs(sentiment.rows[0].value[0] + 1) * 100));
-			
+
 			console.log(Math.round(Math.abs(sentiment.rows[0].value[0] + 1) * 100) / 100);
 			console.log(Math.round(Math.abs(sentiment.rows[0].value[0] - 1) * 100) / 100);
 			var pieData = [
@@ -424,7 +424,7 @@ controllers.controller('QueryCtrl',['$scope', '$http', '$filter', function($scop
 				}
 			  ]
 
-//TODO maybe?: chart isn't updating, it's just redrawing, might need to fix			  
+//TODO maybe?: chart isn't updating, it's just redrawing, might need to fix
 /*
 			pieChart[0].value = Math.round(Math.abs(sentiment.rows[0].value[0] + 1) * 100) / 100;
 			pieChart[1].value = Math.round(Math.abs(sentiment.rows[0].value[0] - 1) * 100) / 100;
@@ -440,9 +440,9 @@ controllers.controller('QueryCtrl',['$scope', '$http', '$filter', function($scop
 			barChart.datasets[0].data[3] = wordCountData[3][1];
 */
 			var barData = {
-				labels: [wordCountData[0][0], 
-					wordCountData[1][0], 
-					wordCountData[2][0], 
+				labels: [wordCountData[0][0],
+					wordCountData[1][0],
+					wordCountData[2][0],
 					wordCountData[3][0]
 				],
 				datasets: [
@@ -452,10 +452,10 @@ controllers.controller('QueryCtrl',['$scope', '$http', '$filter', function($scop
 						strokeColor: "rgba(220,220,220,0.8)",
 						highlightFill: "rgba(220,220,220,0.75)",
 						highlightStroke: "rgba(220,220,220,1)",
-						data: [wordCountData[0][1], 
-						wordCountData[1][1], 
-						wordCountData[2][1], 
-						wordCountData[3][1] 
+						data: [wordCountData[0][1],
+						wordCountData[1][1],
+						wordCountData[2][1],
+						wordCountData[3][1]
 						]
 					}
 				]
@@ -477,23 +477,43 @@ controllers.controller('QueryCtrl',['$scope', '$http', '$filter', function($scop
   }
 
 
+
+	$scope.Letters = function(){
+
+//Math.round(Math.abs(sentiment.rows[0].value[0] + 1) * 100) / 100)
+var Grade = .55;
+var GID= document.getElementById("Grade");
+		if( Grade <= .6){
+		GID.src = "../images/a.png";
+		}
+
+		if( Grade <= .55){
+		GID.src = "../images/b.jpg";
+		}
+
+		if( Grade <= .50){
+		GID.src = "../images/c.jpg";
+		}
+	}
+
+	$scope.Letters();
 }]);
 
 controllers.controller('CompareCtrl',['$scope', function($scope){
-  
+
   // First Pie Graph
   //var pieCtx = document.getElementById("first_pie_canvas").getContext("2d");
   //var pieChart = new Chart(pieCtx).Pie(testPieData);
-  
+
   // First Bar Graph
   //var barCtx = document.getElementById("first_bar_canvas").getContext("2d");
   //var barChart = new Chart(barCtx).Bar(testBarData);
-  
+
   // First Word Cloud
 /*
-  WordCloud(document.getElementById('first_cloud_canvas'), 
-          { 
-              list: testWords, 
+  WordCloud(document.getElementById('first_cloud_canvas'),
+          {
+              list: testWords,
               color: 'random-dark',
               shape: 'square',
               rotateRatio: 0.0,
@@ -504,21 +524,21 @@ controllers.controller('CompareCtrl',['$scope', function($scope){
   // Second Pie Graph
   //var pieCtx = document.getElementById("second_pie_canvas").getContext("2d");
   //var pieChart = new Chart(pieCtx).Pie(testPieData);
-  
+
   // Second Bar Graph
   //var barCtx = document.getElementById("second_bar_canvas").getContext("2d");
   //var barChart = new Chart(barCtx).Bar(testBarData);
-  
+
   // Second Word Cloud
-/*  WordCloud(document.getElementById('second_cloud_canvas'), 
-          { 
-              list: testWords, 
+/*  WordCloud(document.getElementById('second_cloud_canvas'),
+          {
+              list: testWords,
               color: 'random-dark',
               shape: 'square',
               rotateRatio: 0.0,
               weightFactor: 2
           }
   );
-  
+
   */
 }]);
