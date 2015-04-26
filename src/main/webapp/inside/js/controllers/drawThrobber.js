b9991172e18db1c6938ace37784de30e661e20e5
Becca Little <rebecca.little@asu.edu>
2015-04-25 18:14:24 -0700

    made a beautiful throbber for the loading thing :D

---
+++ src/main/webapp/inside/js/controllers/drawThrobber.js
@@ -0,0 +1,29 @@
+function drawThrobber(canvas, start) {
+  
+  var context = canvas.getContext('2d');
+  var lines = 16,  
+      cW = context.canvas.width,
+      cH = context.canvas.height;
+
+  var draw = function() {
+    var rotation = parseInt(((new Date() - start) / 1000) * lines) / lines;
+    context.save();
+    context.clearRect(0, 0, cW, cH);
+    context.translate(cW / 2, cH / 2);
+    context.rotate(Math.PI * 2 * rotation);
+    for (var i = 0; i < lines; i++) {
+        context.beginPath();
+        context.rotate(Math.PI * 2 / lines);
+        context.moveTo(cW / 10, 0);
+        context.lineTo(cW / 4, 0);
+        context.lineWidth = cW / 30;
+        context.strokeStyle = "rgba(0, 0, 0," + i / lines + ")";
+        context.stroke();
+    }
+    context.restore();
+  }
+
+  var throbber = window.setInterval (draw, 1000 / 30);
+  return throbber;
+  
+}
--- src/main/webapp/inside/js/controllers/query.js
+++ src/main/webapp/inside/js/controllers/query.js
@@ -1,3 +1,14 @@
+/* 
+ * The controller for the query page. 
+ *
+ * Uses HTTP to GET makes, models, and years from MySQL,
+ *           to POST a query to CouchDB and thus create a view,
+ *           and to POST a saved query to CouchDB.
+ *
+ * NOTE: Uses jQuery rather than Angular to populate the HTML selects.
+ *       Authors were unable to use Angular to filter selects correctly.
+ */
+
 'use strict';
 
 var notLoaded = true;
@@ -6,8 +17,141 @@ var makes;
 var models;
 var years;
 
-//Properly filter models based on make selected
-var changeMake = function(){
+angular.module('controllers').controller('QueryCtrl', ['$scope', '$http', '$filter',
+                                                      function ($scope, $http, $filter) {
+  
+  // GET data from MySQL and set up make selects using jQuery
+  $scope.getMakes = function() {
+    $http.get('../api/makes').success(function (data) {
+      makes = data;
+      var allMakes = {makeId: -1, makeName: "All Makes"};
+      makes.unshift(allMakes);
+      for (var i = 0; i < makes.length; ++i){
+        $("#selectMake").append($('<option>', {
+          value: makes[i].makeId,
+          text: makes[i].makeName
+        }));
+      }
+      $scope.getModels();
+    });
+  }
+  // GET data from MySQL and set up model selects using jQuery
+  $scope.getModels = function () {
+    $http.get('../api/models').success(function(data) {
+      models = data;
+      var allModels = {makeId: -1, modelId: -1, modelName: "All Models"};
+      models.unshift(allModels); 
+      $("#selectModel").append($('<option>', {
+        value: -1, 
+        text: "All Models"
+      }));
+      $("#selectModel").val(-1);
+      $scope.getYears();
+    });
+  }
+  // GET data from MySQL and set up year selects using jQuery
+  $scope.getYears = function() {
+    $http.get('../api/model-years').success(function(data) {
+      years = data;
+      var allYears = {modelId: -1, yearId: -1, yearName: "All Years"};
+      years.unshift(allYears); 
+      $("#selectYear").append($('<option>', {
+        value: -1, 
+        text: "All Years"
+      }));
+      $("#selectYear").val(-1);
+      $scope.queryPost();
+    });   
+  }
+  
+  
+  // Datepickers
+  $scope.open = function ($event, dateType) {
+    $event.preventDefault();
+    $event.stopPropagation();
+    if (dateType === "start") {
+      $scope.startOpened = true;
+    } else if (dateType === "end") {
+      $scope.endOpened = true;
+    }
+  };
+  // TODO: Would like to add a day to today because of rounding but it messes up format
+  $scope.today = $filter('date')(new Date(), 'yyyy-MM-dd');
+  $scope.endDate = $scope.today;
+  $scope.aMonthAgo = $filter('date')(new Date() - 2592000000, 'yyyy-MM-dd');
+  $scope.startDate = $scope.aMonthAgo;
+
+
+  // POST query to CouchDb, creating a view, and receiving the data in a response
+  // TODO: Because grade is image and pie graph is a canvas, drawing a throbber to 
+  //       the pie canvas makes it off-center. Resolved by creating a new canvas that
+  //       floats over grade and pie and is cleared when the data loads. Is now off-
+  //       center to the left - slightly better, but not awesome.
+  $scope.queryPost = function() {
+    var gradeAndPie = document.getElementById('gradeAndPie_canvas');
+    var pieCtx = gradeAndPie.getContext("2d");
+    var pieChart = document.getElementById('pieGraph_canvas');
+    var wordCloud = document.getElementById('wordCloud_canvas');
+    var barGraph = document.getElementById('barGraph_canvas');
+    
+    var start = new Date();
+    var pieThrob = drawThrobber(gradeAndPie,start);
+    var barThrob = drawThrobber(barGraph,start);
+    var cloudThrob = drawThrobber(wordCloud,start);
+    
+    var payload = { endDate: $scope.endDate,
+      startDate: $scope.startDate,
+      make: $("#selectMake option:selected").text(),
+      model: $("#selectModel option:selected").text(),
+      year: $("#selectYear option:selected").text()
+    }
+    $http({
+      method: 'post',
+      url: '../api/query',
+      headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+      data: $.param(payload)
+    }).then(function(response) {
+      clearInterval(pieThrob);
+      pieCtx.clearRect(0, 0, gradeAndPie.width, gradeAndPie.height);
+      clearInterval(barThrob);
+      clearInterval(cloudThrob);
+      $scope.grade = drawQuery(response, wordCloud, pieChart, barGraph);          
+    });
+  };
+  
+  // POST saved query to CouchDB
+  $scope.saveQuery = function () {
+    var payload = { endDate: $scope.endDate,
+      startDate: $scope.startDate,
+      make: $("#selectMake option:selected").text(),
+      model: $("#selectModel option:selected").text(),
+      year: $("#selectYear option:selected").text(),
+      user: _uToken,
+      searchName: $scope.searchName
+    }
+    $http({
+      method: 'post',
+      url: '../api/savedsearches',
+      headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+      data: $.param(payload)
+    }).then(function(response) {
+      $("#savedMessage").removeClass('hidden');
+      $scope.searchName = "";
+      setTimeout(function(){
+        $("#savedMessage").addClass('hidden');
+      }, 2500)
+    });
+  };
+
+  if (notLoaded) {
+    $scope.getMakes();
+    notLoaded = false;
+  }
+
+}]);
+
+// Use jQuery to properly filter models based on make selected
+var changeMake = function (){
   $("#selectModel").empty();
   $("#selectYear").empty();
 
@@ -29,8 +173,8 @@ var changeMake = function(){
     $("#selectModel").removeAttr('disabled');
   }
 }
-//Properly filter years based on model selected
-var changeModel = function(){
+// User jquery to properly filter years based on model selected
+var changeModel = function (){
   $("#selectYear").empty();
 
   $("#selectYear").append($('<option>', {value: -1, text: "All Years"}));
@@ -47,136 +191,3 @@ var changeModel = function(){
     $("#selectYear").removeAttr('disabled');
   }
 }
-
-angular.module('controllers').controller('QueryCtrl',['$scope', '$http', '$filter', 
-                                                      function($scope, $http, $filter){
-  // Loading stuff
-  var throbber = document.getElementById('throbber');
-  var wordCloud = document.getElementById('wordCloud_canvas');
-  var wCtx=wordCloud.getContext("2d");
-  wCtx.font="20px Calibri";
-  wCtx.fillText("Loading...",10,50);
-  var pieChart = document.getElementById('pieGraph_canvas');
-  var pCtx=pieChart.getContext("2d");
-  pCtx.font="20px Calibri";
-  pCtx.fillText("Loading...",10,50);
-  var barGraph = document.getElementById('barGraph_canvas');
-  var bCtx=barGraph.getContext("2d");
-  bCtx.font="20px Calibri";
-  bCtx.fillText("Loading...",10,50);
-  
-  // Get data from SQL and set up stuff
-  $scope.getMakes = function() {
-    $http.get('../api/makes').success(function(data) {
-      makes = data;
-      var allMakes = {makeId: -1, makeName: "All Makes"};
-      makes.unshift(allMakes);
-
-      for(var i = 0; i < makes.length; ++i){
-        $("#selectMake").append($('<option>', {
-          value: makes[i].makeId,
-          text: makes[i].makeName
-        }));
-      }
-
-      $scope.getModels();
-    });
-  }
-  $scope.getModels = function() {
-    $http.get('../api/models').success(function(data) {
-      models = data;
-      var allModels = {makeId: -1, modelId: -1, modelName: "All Models"};
-      models.unshift(allModels); 
-
-      $("#selectModel").append($('<option>', {value: -1, text: "All Models"}));
-      $("#selectModel").val(-1);
-      $scope.getYears();
-    });
-  }
-  $scope.getYears = function() {
-    $http.get('../api/model-years').success(function(data) {
-      years = data;
-      var allYears = {modelId: -1, yearId: -1, yearName: "All Years"};
-      years.unshift(allYears); 
-
-      $("#selectYear").append($('<option>', {value: -1, text: "All Years"}));
-      $("#selectYear").val(-1);
-      $scope.queryPost();
-    });   
-  }
-  
-  
-  // Datepickers
-  $scope.open = function($event, dateType) {
-    $event.preventDefault();
-    $event.stopPropagation();
-    if (dateType === "start") {
-      $scope.startOpened = true;
-    } else if (dateType === "end") {
-      $scope.endOpened = true;
-    }
-  };
-  // TODO: Would like to add a day to today because of rounding but it messes up format
-  $scope.today = $filter('date')(new Date(), 'yyyy-MM-dd');
-  $scope.endDate = $scope.today;
-  $scope.aMonthAgo = $filter('date')(new Date() - 2592000000, 'yyyy-MM-dd');
-  $scope.startDate = $scope.aMonthAgo;
-
-
-  //POST, query response
-  $scope.queryPost = function() {
-    var payload = { endDate: $scope.endDate,
-      startDate: $scope.startDate,
-      make: $("#selectMake option:selected").text(),
-      model: $("#selectModel option:selected").text(),
-      year: $("#selectYear option:selected").text()
-    }
-    $http({
-      method: 'post',
-      url: '../api/query',
-      headers: {'Content-Type': 'application/x-www-form-urlencoded'},
-      data: $.param(payload)
-    }).then(function(response) {
-      var wordCloud = document.getElementById('wordCloud_canvas');
-      var pieChart = document.getElementById('pieGraph_canvas');
-      var barGraph = document.getElementById('barGraph_canvas');
-      $scope.grade = drawQuery(response, wordCloud, pieChart, barGraph);          
-    });
-  };
-  
-
-
-  $scope.saveQuery = function() {
-    var payload = { endDate: $scope.endDate,
-      startDate: $scope.startDate,
-      make: $("#selectMake option:selected").text(),
-      model: $("#selectModel option:selected").text(),
-      year: $("#selectYear option:selected").text(),
-      user: _uToken,
-      searchName: $scope.searchName
-    }
-
-    $http({
-      method: 'post',
-      url: '../api/savedsearches',
-      headers: {'Content-Type': 'application/x-www-form-urlencoded'},
-      data: $.param(payload)
-    }).then(function(response) {
-      $("#savedMessage").removeClass('hidden');
-      $scope.searchName = "";
-      setTimeout(function(){
-        $("#savedMessage").addClass('hidden');
-      }, 2500)
-      console.log("response");
-      console.log(response);
-    });
-  };
-
-  if (notLoaded) {
-    $scope.getMakes();
-    notLoaded = false;
-  }    
-
-}]);
-
-
